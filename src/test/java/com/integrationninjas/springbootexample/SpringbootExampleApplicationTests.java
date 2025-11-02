e091906bc2a9339bec251c8e8b25d308868f63ea
djinislav <denislavdanov@gmail.com>
2025-11-02 17:03:30 +0200

    add unit tests

--- src/test/java/com/integrationninjas/springbootexample/SpringbootExampleApplicationTests.java
+++ src/test/java/com/integrationninjas/springbootexample/SpringbootExampleApplicationTests.java
@@ -1,13 +1,24 @@
-package com.integrationninjas.springbootexample;
+package com.integrationninjas.springbootexample.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class SpringbootExampleApplicationTests {
+@WebMvcTest(TestController.class)
+class TestControllerTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void hello_ReturnsExpectedJson() throws Exception {
+        mockMvc.perform(get("/")) // sends an HTTP GET request to the endpoint defined in your controller’s @GetMapping.
+                .andExpect(status().isOk()) // status 200 - ok
+                .andExpect(jsonPath("$.name").value("djinislav"))
+                .andExpect(jsonPath("$.email").value("denislavdanov@gmail.com"));
+    }
 }
